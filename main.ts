b30fb924c8b692509465fb8594e9d86d8c14d16e
NewtonVoss <77486794+NewtonVoss@users.noreply.github.com>
2022-04-01 09:51:59 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,14 +9,5 @@ radio.onReceivedString(function (receivedString) {
     basic.pause(1000)
     basic.clearScreen()
 })
-input.onButtonPressed(Button.B, function () {
-    Sendereffekt += 1
-    radio.setTransmitPower(Sendereffekt)
-    basic.showNumber(Sendereffekt)
-    basic.pause(500)
-    basic.clearScreen()
-})
-let Sendereffekt = 0
 radio.setGroup(2)
-Sendereffekt = 0
-radio.setTransmitPower(Sendereffekt)
+radio.setTransmitPower(2)
